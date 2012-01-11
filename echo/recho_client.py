5d54e4abda4fb52bd5c0d373c0cfafd08d2fa50b
Diana Martinez <diamarely@yahoo.com>
2012-01-10 20:47:20 -0800

    adding new recho client and server

---
+++ echo/recho_client.py
@@ -0,0 +1,36 @@
+"""
+echo client, usage:
+
+ python echo_client.py <host> <port>
+
+Both host and port are optional, defaults: localhost 50000
+host must be present if you want to provide port
+"""
+
+import socket 
+import sys
+
+host = 'localhost' 
+port = 60000 
+size = 1024 
+
+nargs = len(sys.argv)
+if nargs > 1:
+    host = sys.argv[1]
+if nargs > 2:
+    port = int(sys.argv[2])
+
+s = socket.socket(socket.AF_INET, 
+                  socket.SOCK_STREAM) 
+s.connect((host,port))
+mymes = raw_input("Enter something that is text: ")
+while mymes:
+    s.send(mymes) 
+    data = s.recv(size) 
+    s.close() 
+    print 'from (%s,%s) %s' % (host, port, data)
+    mymes = raw_input("Enter something that is text: ")
+    s = socket.socket(socket.AF_INET, 
+                  socket.SOCK_STREAM)
+    s.connect((host,port))
+
